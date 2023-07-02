ab6ac6418e0847bd853a8f986d6973b938d64bb2
Eric Wang <beatofangel@gmx.com>
2023-07-03 00:53:03 +0800

    prisma seed update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -16,7 +16,7 @@ async function main() {
       password: '$2b$10$VPdbcpGN54JAO72kjb49Pu8i8qLKVRyHt3KxZyiMWf4IzKbKmi3Lu',
       name: 'administrator',
       email: null,
-      image: null,
+      image: 'https://api.multiavatar.com/administrator.png',
     },
   })
   const testUser = await prisma.user.upsert({
@@ -26,9 +26,9 @@ async function main() {
       id: 2,
       username: 'test-user',
       password: '$2b$10$VPdbcpGN54JAO72kjb49Pu8i8qLKVRyHt3KxZyiMWf4IzKbKmi3Lu',
-      name: 'test user',
+      name: '张三',
       email: null,
-      image: null,
+      image: 'https://api.multiavatar.com/test-user.png',
     },
   })
   console.log({ admin, testUser })
@@ -40,7 +40,7 @@ async function main() {
     create: {
       id: 1,
       ptype: 'p',
-      v0: 'role_admin',
+      v0: 'role_base',
       v1: '/',
       v2: 'read',
       v3: null,
@@ -48,53 +48,95 @@ async function main() {
       v5: null
     }
   })
-  const roleAdminWrite = await prisma.casbinRule.upsert({
+  const roleReplacementRead = await prisma.casbinRule.upsert({
     where: { id: 2 },
     update: {},
     create: {
       id: 2,
       ptype: 'p',
-      v0: 'role_admin',
-      v1: '/',
-      v2: 'write',
-      v3: null,
-      v4: null,
-      v5: null
-    }
-  })
-  const roleUserRead = await prisma.casbinRule.upsert({
-    where: { id: 3 },
-    update: {},
-    create: {
-      id: 3,
-      ptype: 'p',
-      v0: 'role_user',
-      v1: '/',
+      v0: 'role_replacement',
+      v1: '/replacement',
       v2: 'read',
       v3: null,
       v4: null,
       v5: null
     }
   })
-  const roleUserWrite = await prisma.casbinRule.upsert({
-    where: { id: 4 },
+  const roleOtherRead = await prisma.casbinRule.upsert({
+    where: { id: 3 },
     update: {},
     create: {
-      id: 4,
+      id: 3,
       ptype: 'p',
-      v0: 'role_user',
-      v1: '/',
-      v2: 'write',
+      v0: 'role_other',
+      v1: '/prisma',
+      v2: 'read',
       v3: null,
       v4: null,
       v5: null
     }
   })
-  const roleAdminGroup = await prisma.casbinRule.upsert({
+  const roleUserGroupBase = await prisma.casbinRule.upsert({
+    where: { id: 4 },
+    update: {},
+    create: {
+      id: 4,
+      ptype: 'g',
+      v0: 'role_user',
+      v1: 'role_base',
+      v2: null,
+      v3: null,
+      v4: null,
+      v5: null
+    }
+  })
+  const roleUserGroupReplacement = await prisma.casbinRule.upsert({
     where: { id: 5 },
     update: {},
     create: {
       id: 5,
+      ptype: 'g',
+      v0: 'role_user',
+      v1: 'role_replacement',
+      v2: null,
+      v3: null,
+      v4: null,
+      v5: null
+    }
+  })
+  const roleAdminGroupUser = await prisma.casbinRule.upsert({
+    where: { id: 6 },
+    update: {},
+    create: {
+      id: 6,
+      ptype: 'g',
+      v0: 'role_admin',
+      v1: 'role_user',
+      v2: null,
+      v3: null,
+      v4: null,
+      v5: null
+    }
+  })
+  const roleAdminGroupOther = await prisma.casbinRule.upsert({
+    where: { id: 7 },
+    update: {},
+    create: {
+      id: 7,
+      ptype: 'g',
+      v0: 'role_admin',
+      v1: 'role_other',
+      v2: null,
+      v3: null,
+      v4: null,
+      v5: null
+    }
+  })
+  const roleAdmin = await prisma.casbinRule.upsert({
+    where: { id: 8 },
+    update: {},
+    create: {
+      id: 8,
       ptype: 'g',
       v0: 'admin',
       v1: 'role_admin',
@@ -104,11 +146,11 @@ async function main() {
       v5: null
     }
   })
-  const roleUserGroup = await prisma.casbinRule.upsert({
-    where: { id: 6 },
+  const roleTestUser = await prisma.casbinRule.upsert({
+    where: { id: 9 },
     update: {},
     create: {
-      id: 6,
+      id: 9,
       ptype: 'g',
       v0: 'test-user',
       v1: 'role_user',
@@ -118,7 +160,7 @@ async function main() {
       v5: null
     }
   })
-  console.log({ roleAdminRead, roleAdminWrite, roleUserRead, roleUserWrite, roleAdminGroup, roleUserGroup })
+  console.log({ roleAdminRead, roleReplacementRead, roleOtherRead, roleUserGroupBase, roleUserGroupReplacement, roleAdminGroupUser, roleAdminGroupOther, roleAdmin, roleTestUser })
 }
 
 main()
