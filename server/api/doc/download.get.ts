3cb63968f70dbd2a010079e405cf266073e01725
Eric Wang <beatofangel@gmx.com>
2023-07-11 23:37:39 +0800

    add console log

--- server/api/doc/download.get.ts
+++ server/api/doc/download.get.ts
@@ -1,12 +1,14 @@
 import fs from 'fs'
 import mime from 'mime'
 import jwt from 'jsonwebtoken'
+import path from 'path'
 const {
   cfgSignatureSecret,
   cfgSignatureAuthorizationHeader,
   cfgSignatureAuthorizationHeaderPrefix,
   cfgSignatureEnable,
   cfgSignatureUseForRequest,
+  storageConfigFolder
 } = useDocConfig()
 
 export default defineEventHandler(async (event) => {
@@ -36,14 +38,15 @@ export default defineEventHandler(async (event) => {
   //   }
   // }
 
-  let path = docManager.forcesavePath(filename, false) ?? docManager.storagePath(filename);  // get the path to the force saved document version
-  console.log('download file from:', path)
+  console.log('working dir:', path.resolve(storageConfigFolder))
+  let path2 = docManager.forcesavePath(filename, false) ?? docManager.storagePath(filename);  // get the path to the force saved document version
+  console.log('download file from:', path2)
 
-  res.setHeader("Content-Length", fs.statSync(path).size);  // add headers to the response to specify the page parameters
-  res.setHeader("Content-Type", mime.getType(path)!);
+  res.setHeader("Content-Length", fs.statSync(path2).size);  // add headers to the response to specify the page parameters
+  res.setHeader("Content-Type", mime.getType(path2)!);
 
   res.setHeader("Content-Disposition", "attachment; filename*=UTF-8\'\'" + encodeURIComponent(filename));
 
-  var filestream = fs.createReadStream(path);
+  var filestream = fs.createReadStream(path2);
   filestream.pipe(res);  // send file information to the response by streams
 })
